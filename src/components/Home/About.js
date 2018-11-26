5db703b13c7ed3cd8b798bd2bb6ad0d151d3eddb
Robert L <1998.robert@gmail.com>
2018-11-25 22:41:13 -0500

    Add basic text and divs, still need to add img and left-align text

--- src/components/Home/About.js
+++ src/components/Home/About.js
@@ -8,13 +8,40 @@ const Wrapper = styled.div`
 	align-items: center;
 	padding-left: 50px;
 	padding-right: 50px;
+	flex: 1;
+`
+
+const Header = styled.div`
+	background-color: ${colors.primary};
+	padding: 1em;
+	justify-content: flex-start;
+`
+
+const Title = styled.h1`
+  font-size: 1.3rem;
+  text-align: left;
+`
+
+const Body = styled.div`
+	background-color: ${colors.secondary};
+	padding: 12em;
+	text-align: center;
 `
 
 class About extends Component {
   render() {
     return (
       <Wrapper>
-        
+				<Header>
+					<Title>
+						Learn to build and ship applications
+					</Title>
+					bridging the gap between CS education and technical entrepreneurship at Yale
+				</Header>
+				<Body>
+					We turn creative students into <br></br>
+					technical superstars
+				</Body>
       </Wrapper>
     );
   }
